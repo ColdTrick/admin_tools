48ce34f7f6b66b829137942f1737f8817df386f1
Jerome Bakker <jeabakker@coldtrick.com>
2012-03-12 09:18:41 +0100

    added: memory usage

--- views/default/admin_tools/footer.php
+++ views/default/admin_tools/footer.php
@@ -1,13 +1,15 @@
 <?php
 	global $dbcalls, $START_MICROTIME;
 	$num_included_files = count(get_included_files());
+	$max_mem = number_format(memory_get_peak_usage(true), 0, ",", ".");
+	$time = number_format(microtime(true) - $START_MICROTIME, 5, ",", ".");
 	
 ?>
 <div id="admin_tools_stats_box">
 	<table>
 		<tr>
 			<td>PHP servertime: </td>
-			<td><?php echo (microtime(true) - $START_MICROTIME); ?></td>
+			<td><?php echo $time; ?></td>
 		</tr>
 		<tr>
 			<td>DB calls: </td>
@@ -16,6 +18,10 @@
 		<tr>
 			<td>included files: </td>
 			<td><?php echo $num_included_files; ?></td>
+		</tr>
+		<tr>
+			<td>Max memory used: </td>
+			<td><?php echo $max_mem; ?> bytes</td>
 		</tr>
 	</table>
 </div>
