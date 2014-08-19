b2f1eec0a89f7213e5e02e1564069f96424d7b99
Jerome Bakker <jeabakker@coldtrick.com>
2014-08-19 14:19:31 +0200

    language update

--- languages/en.php
+++ languages/en.php
@@ -5,7 +5,7 @@ return array(
 	'admin_tools:switch_to_user' => "Act as user",
 	'admin_tools:switch_to_admin' => "Act as admin",
 	
-	'admin_tools:switched_admins:title' => "Switched admins",
+	'admin_tools:switched_admins:title' => "Admins who have temporarily switched to a normal user",
 	'admin_tools:switched_admins:none' => "Currently there are no admins who switched to normal user",
 	
 	// actions
---
+++ languages/nl.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file was created by Translation Editor v2.0
+ * On 2014-08-19 14:16
+ */
+
+$language = array (
+  'admin_tools:switch_to_user' => 'Wordt gebruiker',
+  'admin_tools:switch_to_admin' => 'Wordt beheerder',
+  'admin_tools:switched_admins:title' => 'Beheerders die tijdelijk gewone gebruiker zijn',
+  'admin_tools:switched_admins:none' => 'Op dit moment zijn er geen beheerders die tijdelijk gewone gebruiker zijn',
+  'admin_tools:action:toggle_admin:success:user' => 'Je bent nu een normale gebruiker',
+  'admin_tools:action:toggle_admin:success:admin' => 'Je bent nu weer een beheerder',
+);
+add_translation("nl", $language);
