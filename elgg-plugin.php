4bfe65811ebf7792dafb61f94d78aa00dac54e2f
Jerôme Bakker <jeabakker@coldtrick.com>
2023-07-26 15:02:22 +0200

    chore: wrapping up v7.0.1

--- elgg-plugin.php
+++ elgg-plugin.php
@@ -4,7 +4,7 @@ require_once(dirname(__FILE__) . '/lib/functions.php');
 
 return [
 	'plugin' => [
-		'version' => '7.0',
+		'version' => '7.0.1',
 	],
 	'settings' => [
 		'deadlink_include_skipped_domains' => 1,
